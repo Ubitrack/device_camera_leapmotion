0c4136804254cc9c1b3aeea42d6b79fd62904b94
Frieder Pankratz <pankratz@in.tum.de>
2019-11-12 15:21:11 +0100

    changes for conan build

---
+++ conanfile.py
@@ -0,0 +1,55 @@
+from conans import ConanFile, CMake, tools
+
+
+class UbitrackCoreConan(ConanFile):
+    name = "ubitrack_device_camera_leapmotion"
+    version = "1.3.0"
+
+    description = "Ubitrack Device Camera Leap motion"
+    url = "https://github.com/Ubitrack/device_leapmotion.git"
+    license = "LGPL"
+
+    short_paths = True
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    options = {
+        "workspaceBuild" : [True, False],
+    }
+
+    default_options = {
+        "workspaceBuild" : False,
+        "ubitrack_core:shared" : True,
+        "ubitrack_vision:shared" : True,
+        "ubitrack_dataflow:shared" : True,
+    }
+        
+
+    # all sources are deployed with the package
+    exports_sources = "cmake/*", "doc/*", "src/*", "CMakeLists.txt"
+
+    def requirements(self):
+        userChannel = "ubitrack/stable"
+        if self.options.workspaceBuild:
+            userChannel = "local/dev"
+        self.requires("ubitrack_core/%s@%s" % (self.version, userChannel))
+        self.requires("ubitrack_vision/%s@%s" % (self.version, userChannel))
+        self.requires("ubitrack_dataflow/%s@%s" % (self.version, userChannel))
+       
+
+    # def imports(self):
+    #     self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+    #     self.copy(pattern="*.dylib*", dst="lib", src="lib") 
+    #     self.copy(pattern="*.so*", dst="lib", src="lib") 
+       
+    def build(self):
+        cmake = CMake(self)        
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        pass
